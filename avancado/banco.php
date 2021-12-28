bd052648aa4032beca2993604c1844fccd77ffd0
Matheus Bueno <bueno_matheus@outlook.com>
2021-12-28 09:44:24 -0300

    Adicionando a função de depositar em banco.php

--- avancado/banco.php
+++ avancado/banco.php
@@ -1,20 +1,5 @@
 <?php
 
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
-}
-
-function sacar($conta, $valorASacar)
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -30,9 +15,55 @@ $contasCorrentes = [
     ]
 ];
 
+function exibeMensagem($mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
 
-$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
+function depositar(array $conta, float $valorADepositar): array 
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depósitos precisam ser positivos");
+    }
+
+    return $conta;
+}
+
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+
+$contasCorrentes['123.256.789-12'] = depositar(
+    $contasCorrentes['123.256.789-12'],
+    900
+);
+
+
+$contasCorrentes['123.456.789-10'] = sacar(
+    $contasCorrentes['123.456.789-10'],
+    500
+);
+
+$contasCorrentes['123.456.789-11'] = sacar(
+    $contasCorrentes['123.456.789-11'], 
+    200
+);
+
+
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+    exibeMensagem(
+        "$cpf {$conta['titular']} {$conta['saldo']}"
+    );
 }
