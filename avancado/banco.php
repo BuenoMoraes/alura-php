8f4767ba85d8556b97d7f52ca13cd546b99a0a6c
Matheus Bueno <bueno_matheus@outlook.com>
2021-12-28 10:18:48 -0300

    Adicionando o arquivo funcoes.php

--- avancado/banco.php
+++ avancado/banco.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -14,34 +16,6 @@ $contasCorrentes = [
         'saldo' => 100
     ]
 ];
-
-function exibeMensagem($mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function depositar(array $conta, float $valorADepositar): array 
-{
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-
-    return $conta;
-}
-
-
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
 
 
 $contasCorrentes['123.256.789-12'] = depositar(
